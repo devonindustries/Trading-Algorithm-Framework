21f98076e267a156bfd5f690c23e9aa81a8ed43c
Joshua Baker <55383175+devonindustries@users.noreply.github.com>
2021-03-10 14:29:49 +0000

    Re-designing 'Portfolio' class
    In the process of changing the portfolio class to work with the Stock class.

--- trading_algorithm_framework/portfolio.py
+++ trading_algorithm_framework/portfolio.py
@@ -1,183 +1,234 @@
 # Define a class to handle a share in a stock
 class Share:
     '''
-    Create a new share in a stock that can be added to the users portfolio. Takes four arguments:
+    Create a new share in a stock that can be added to the users portfolio. Takes 2 arguments:
 
     - price : The price at the time of purchasing the share;
-    - volume : The volume of shares purchased;
-    - date : The date that the stock was purchased;
-    - symbol (optional) : The symbol of the stock. Empty by default.
+    - volume : The volume of shares purchased.
     '''
-    
-    # Declare the id attribute before assignment for validation
-    __id = None
     
     #----------------
     # Built-in Methods
     #----------------
     
-    def __init__(self, price, volume, stock_date, symbol=None):
+    def __init__(self, price, volume):
         self.price = price
         self.volume = volume
-        self.stock_date = stock_date
-        self.symbol = symbol
+
+
+# Define a class to handle the positions for a given symbol. This will be handled by the Portfolio class
+class Asset:
+    '''
+    Create a new instance of the asset class to handle the users positions for any given symbol
+    '''
+
+    # The users statistics for a given ticker
+    exposure = 0
+    gains = 0
+
+    # The positions that the user has entered, and an identical history dictionary
+    positions, history = {
+
+        # Define dictionaries to hold the users long and short positions in the given stock
+        'long' : dict(),
+        'short' : dict(),
+
+        # Define dictionaries for call and put options (support will be added in future versions)
+        'call' : dict(),
+        'put' : dict(),
+    }
     
+    #----------------
+    # Built-in Methods
+    #----------------
+
+    def __init__():
+        pass
+
+    #----------------
+    # Private Methods
+    #----------------
+
+    def __calculate_stats():
+        pass
+
     #----------------
     # Public Methods
     #----------------
-                
-    # Set a private id attribute using a public method to avoid overwriting
-    def set_id(self, id):
-        if not(self.__id):
-            self.__id = id
-          
-    # Public method to return the id
-    def get_id(self):
-        return self.__id
-          
+
+    def enter_position(self, asset_type, share, entry_datetime):
+
+        # Append the new share in the relevant list
+        self.positions[asset_type][entry_datetime] = share
+
+
+    def leave_position(self, asset_type, current_price, volume, entry_datetime):
         
+        # Bound the volume
+        stock_volume = self.positions[asset_type][entry_datetime].volume
+        if volume > stock_volume: volume = stock_volume 
+
+
 # Define a class to handle the stock information in the users portfolio
 class Portfolio:
     '''
     Create a new portfolio to purchase shares with.
     
-    ! Note: Currently there is no suppoprt for stock timestamps, only dates. Support may be added in later versions.
+    Takes 2 arguments:
 
-    Values that may be allocated when calling this class:
-
-    - starting_balance = 50 000 (by default)
+    - balance (optional) : The money that the account begins with. Set to 50 000 by default;
+    - symbols (optional) : A list containing all of the symbols that the user wishes to trade with. Set to nothing by default, but can be changed later.
     '''
-    
-    # Store all of the portfolio short and long positions
-    short_ = []
-    long_ = []
 
-    # Define private attributes to dynamically allocate IDs to shares
-    # This will start at zero, and will only ever be incremented
-    __short_id = 0
-    __long_id = 0
-    
+    # Declare a dictionary for each desired symbol
+    positions = dict()
+
     #----------------
-    # Private Methods
+    # Built-in Methods
     #----------------
-    
-    # An initiation function with $50k start money
-    def __init__(self, balance=50000):
+
+    def __init__(self, balance=50000, symbols=None):
         self.balance = balance
-        
-    # Call this every time a position is entered
-    def __increment_id(self, is_long):
-        if is_long:
-            self.__long_id += 1
-        else:
-            self.__short_id += 1
-    
-    # Call this to get the position of a share with a given id    
-    def __get_from_id(self, search_id, is_long):
-        
-        # Get the array in question
-        shlo = self.long_ if is_long else self.short_
-        
-        # Return position in array 
-        i = 0
-        
-        # Loop until we find a stock with the given id
-        while not(pos):
+
+        # Add the symbols if the user passed in a list
+        if type(symbols) == list: 
+            for symbol in symbols: 
+                self.add_symbol(symbol)
           
-          # Return if the id matches
-          if shlo[i].get_id() == search_id:
-            return i
-          
-    
     #----------------
     # Public Methods
     #----------------
-    
-    # A function to reset the user cash
+
     def reset_balance(self, new_balance=50000): 
         '''
-        Resets the user balance. Takes one argument:
+        Resets the user balance. Takes 1 argument:
 
-        - (new_balance) : Set to 50 000 by default.
+        - new_balance (optional) : Set to 50 000 by default.
         '''
+
+        # Set the balance
         self.balance = new_balance
-        
-    # A function to add a share to the users portfolio
-    def buy(self, share, is_long):
+
+
+    def add_symbol(self, symbol, overwrite=False):
         '''
-        Enters a position. Takes two arguments:
+        Add a new symbol to the positions dictionary. Takes 2 arguments:
 
-        - share : An instance of the share class for the stock you wish to purchase;
-        - is_long : A boolean which tells the portfolio to store the share as a long or short position.
+        - symbol : The name of the symbol to be added;
+        - overwrite (optional) : If the symbol exists, set this to true to overwrite it. Set to False by default.
         '''
-      
-        # Reference the array in question
-        shlo = self.long_ if is_long else self.short_
-        
-        # Set the id incrementer
-        temp_id = self.__long_id if is_long else self.__short_id
-        
-        # Set the id of the share
-        share.set_id(temp_id)
-        
-        # Increment the correct id
-        self.__increment_id(is_long)
-        
-        # Append to the correct array
-        shlo.append(share)
-        
-        # If the position is long, deduct from the balance
-        if is_long:
-            self.balance -= share.price * share.volume
-            
-    # A function to sell a position with a given id
-    def sell(self, share_id, volume, current_price, is_long):
+
+        # Replace the asset unless the user does not want to overwrite, and the symbol exists
+        if not(symbol in self.positions.keys()) or overwrite: self.positions[symbol] = Asset()
+
+
+    def remove_symbol(self, symbol):
+
+        # Check that the symbol is in the keys list
+        if symbol in self.positions.keys():
+
+            # Remove the item
+            self.positions.pop(symbol)
+
+    
+    def buy(self, symbol, asset_type, share, entry_datetime):
         '''
-        Leaves a postion. Takes four arguments:
+        Enters a position. Takes 4 arguments:
 
-        - share_id : The id of the share that the user wishes to sell;
-        - volume : The number of shares that the user wishes to purchase. Max volume will be sold if this exceeds the existing volume;
-        - current_price : The value of the share on the date assigned;
-        - is_long : A boolean which tells the portfolio to store the share as a long or short position.
+        - symbol : The stock symbol that the user is buying stock with;
+        - asset_type : The type of position that the user wishes to enter. Takes 2 possible values:
+            - 'long' : Enter a long position;
+            - 'short' : Enter a short position;
+        - share : An instance of the share class that the user is entering the position with;
+        - entry_datetime : The datetime object associated with the time the position was entered.
         '''
-        
-        # Reference the array in question
-        shlo = self.long_ if is_long else self.short_
-        
-        # Get the position in the array that holds the desired stock
-        pos = self.__get_from_id(share_id, is_long)
-        
-        # Bound the volume if it is too high
-        if volume > shlo[pos].volume: volume = shlo[pos].volume
+        pass
 
-        # Now sell the stock depending on the position
-        balance += volume * current_price if is_long else volume * (shlo[pos].price - current_price)
 
-        # Remove the stock from the users portfolio if the maximum volume was sold, or adjust the volume
-        if volume == shlo[pos].volume:
-            shlo.pop(pos)
-        else:
-            shlo[pos].volume -= volume
-  
-    # Sell all the stock held by the portfolio
-    def sell_all(self, current_price, is_long):
+    def sell(self, symbol, asset_type, current_price, volume, entry_datetime):
         '''
-        Leaves all positions for a given position type. Takes two arguments:
+        Leaves a position. Takes 3 arguments:
 
-        - current_price : The value of the share when the method is called
+        - symbol : The symbol of the asset being sold;
+        - asset_type : The type of position the user wishes to leave. Takes 2 possible values:
+            - 'long' : Enter a long position;
+            - 'short' : Enter a short position;
+        - current_price : The current price of the stock;
+        - volume : The volume of stock that the user wishes to sell.
+        - entry_datetime : The datetime object associated with the time that the position was entered.
         '''
-        
-        # Reference the array in question
-        shlo = self.long_ if is_long else self.short_
+        pass
 
-        # If the array is empty, then return
-        if len(shlo) == 0: return
 
-        # Loop through all of the positions in the array
-        for i in range(0, len(shlo)):
+    def sell_all(self, symbol, asset_type):
+        '''
+        Leaves all positions. Takes 2 arguments:
 
-            # Get the stock id
-            temp_id = shlo[0].get_id()
+        - symbol : The symbol of the asset being sold;
+        - asset_type : The type of position the user wishes to leave. Takes 2 possible values:
+            - 'long' : Enter a long position;
+            - 'short' : Enter a short position.
+        '''
+        pass
 
-            # Delete the stock with that id
-            self.sell(temp_id, shlo[0].volume, current_price, is_long)
+    
+
+
+#     # A function to add a share to the users portfolio
+#     def buy(self, share, is_long):
+#         '''
+#         Enters a position. Takes two arguments:
+#         - share : An instance of the share class for the stock you wish to purchase;
+#         - is_long : A boolean which tells the portfolio to store the share as a long or short position.
+#         '''     
+#         # Reference the array in question
+#         shlo = self.long_ if is_long else self.short_      
+#         # Set the id incrementer
+#         temp_id = self.__long_id if is_long else self.__short_id      
+#         # Set the id of the share
+#         share.set_id(temp_id)     
+#         # Increment the correct id
+#         self.__increment_id(is_long)      
+#         # Append to the correct array
+#         shlo.append(share)
+#         # If the position is long, deduct from the balance
+#         if is_long:
+#             self.balance -= share.price * share.volume
+#     # A function to sell a position with a given id
+#     def sell(self, share_id, volume, current_price, is_long):
+#         '''
+#         Leaves a postion. Takes four arguments:
+#         - share_id : The id of the share that the user wishes to sell;
+#         - volume : The number of shares that the user wishes to purchase. Max volume will be sold if this exceeds the existing volume;
+#         - current_price : The value of the share on the date assigned;
+#         - is_long : A boolean which tells the portfolio to store the share as a long or short position.
+#         '''      
+#         # Reference the array in question
+#         shlo = self.long_ if is_long else self.short_      
+#         # Get the position in the array that holds the desired stock
+#         pos = self.__get_from_id(share_id, is_long)     
+#         # Bound the volume if it is too high
+#         if volume > shlo[pos].volume: volume = shlo[pos].volum
+#         # Now sell the stock depending on the position
+#         balance += volume * current_price if is_long else volume * (shlo[pos].price - current_price)
+#         # Remove the stock from the users portfolio if the maximum volume was sold, or adjust the volume
+#         if volume == shlo[pos].volume:
+#             shlo.pop(pos)
+#         else:
+#             shlo[pos].volume -= volume
+#     # Sell all the stock held by the portfolio
+#     def sell_all(self, current_price, is_long):
+#         '''
+#         Leaves all positions for a given position type. Takes two arguments:
+#         - current_price : The value of the share when the method is called
+#         '''     
+#         # Reference the array in question
+#         shlo = self.long_ if is_long else self.short_
+#         # If the array is empty, then return
+#         if len(shlo) == 0: return
+#         # Loop through all of the positions in the array
+#         for i in range(0, len(shlo)):
+#             # Get the stock id
+#             temp_id = shlo[0].get_id()
+#             # Delete the stock with that id
+#             self.sell(temp_id, shlo[0].volume, current_price, is_long)
